9436d9eb455d06aa08ac32abc4101e4de37100b4
Kamil Krukowski <hadekshd@gmail.com>
2025-07-14 18:13:18 +0200

    Delete unused section

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -3,8 +3,8 @@ import { AdminDashboard } from "../components/admin/AdminDashboard";
 import { Layout } from "../components/Layout";
 import { ProtectedRoute } from "../components/ProtectedRoute";
 import { SupabaseSetup } from "../components/upload/SupabaseSetup";
-import { debugSession } from "../services/supabaseStorage"; 
 import { productService } from "../services/productService";
+import { debugSession } from "../services/supabaseStorage";
 import { isUserAdmin } from "../utils/adminUtils";
 
 // Add debug logging
@@ -15,12 +15,12 @@ export const AdminPage: React.FC = () => {
   // Debug session state when admin page loads
   React.useEffect(() => {
     debugSession();
-    
+
     const checkAdminStatus = async () => {
-        // Check admin status - product list is managed by AdminDashboard
-        console.log('Admin page loaded');
+      // Check admin status - product list is managed by AdminDashboard
+      console.log("Admin page loaded");
     };
-    
+
     checkAdminStatus();
   }, []);
 
@@ -28,9 +28,6 @@ export const AdminPage: React.FC = () => {
     <ProtectedRoute requireAdmin>
       <Layout>
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-          <div className="mb-8">
-            <SupabaseSetup />
-          </div>
           <AdminDashboard />
         </div>
       </Layout>
